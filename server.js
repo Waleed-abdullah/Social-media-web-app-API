a5e82bb3f7851dd547a96ab5977e2d74137fb227
Nouman Amir <noumanamir453@gmail.com>
2021-12-10 22:19:30 +0500

    Added post image functionality

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ import { createConnection } from 'mysql2';
 import cors from 'cors';
 import handleSignIn from './controllers/handleSignIn.js';
 import handleSaveProfile from './controllers/handleSaveProfile.js';
+import multer from 'multer'
+import path from 'path'
 
+const __dirname = path.resolve()
 const app = express();
 app.use(json());
 app.use(cors());
+
+app.use('/retrieve', express.static(path.join(__dirname, '/public/images')));
 
 const connection = createConnection({
   host: process.env.SQL_HOST,
@@ -32,6 +37,58 @@ connection.connect((err) => {
     console.log('Connected');
   }
 });
+
+let storage = multer.diskStorage({
+  destination: (req, file, callBack) => {
+      callBack(null, './public/images/')     // './public/images/' directory name where save the file
+  },
+  filename: (req, file, callBack) => {
+      callBack(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+})
+
+let upload = multer({
+  storage: storage
+});
+
+app.post("/upload", upload.single('file'), (req, res) => {
+  if (!req.file) {
+      console.log("No file upload");
+  } else {
+      console.log(req.file.filename)
+      let imgsrc = req.file.filename
+      let userID = "nansjansjansjnd"
+      let insertData = 'INSERT INTO `post` (postImage) VALUES(?)'
+      connection.query(insertData, [imgsrc, userID], (err, result) => {
+          if (err) {throw err}
+          console.log("file uploaded")
+          res.status(201).send(req.file.filename)
+      })
+  }
+});
+
+/*
+app.get("/retrieve", (req, res) => {
+  const id = 1;
+  const sqlQ = "SELECT postImage FROM post WHERE postID = 6"
+
+  connection.query(sqlQ, [id], (err, result) => {
+    if (err){
+      console.log(err)
+      res.send({
+        msg: err
+      })
+    }
+
+    if (result){
+      console.log(result[0].postImage)
+      res.send({
+        image: result[0].postImage
+      })
+    }
+  })
+})
+*/
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening to localhost:${port}`));
